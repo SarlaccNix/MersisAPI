91a4e84fbfd82f3d2ec7398867b7cd4ca134070e
Hernan <whiterabbitbuilder@gmail.com>
2022-08-03 18:10:48 -0500

    Update upload file data

--- MapsAPI/Controllers/MapsController.cs
+++ MapsAPI/Controllers/MapsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using MongoDB.Bson;
 using MapsAPI.Models;
+using Newtonsoft.Json;
 
 namespace MapsAPI.Controllers;
 
@@ -37,6 +38,12 @@ public class MapsController : ControllerBase
     [HttpPost("createNewMap")]
     public IEnumerable<Maps> Post()
     {
+        var payload = String.Empty;
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            payload = reader.ReadToEndAsync().Result;
+        }
+        dynamic payloadData = JsonConvert.DeserializeObject<dynamic>(payload);
         var client = new MongoClient("mongodb+srv://whiteRabbit:MaudioTest@cluster0.u4jq0.mongodb.net/test");
         var database = client.GetDatabase("QH_Maps_Default");
         var filePath = @"F:\Repo\Mersis\Port_City.zip";
@@ -45,18 +52,28 @@ public class MapsController : ControllerBase
 
         var binary = new BsonElement();
         var defaultMapsDb = database.GetCollection<Maps>("QH_Maps");
-        var map = new Maps() { MapName = "Tavern", CreatorId = "Blindy", Id = "Blindy_1_PowerAdmin", MapFile = fileByteArray};
-       System.IO.File.WriteAllBytes(@"F:\Repo\Mersis\OutputFolder\Port_City_output.zip", (fileByteArray));
-        return Enumerable.Range(1, 5).Select(index => new Maps
+        var map = new Maps() { MapName = payloadData.mapName, CreatorId = payloadData.creatorId, Id = payloadData.id, MapFile = fileByteArray };
+        System.IO.File.WriteAllBytes(@"F:\Repo\Mersis\OutputFolder\Port_City_output.zip", (fileByteArray));
+        try
+        {
+            defaultMapsDb.InsertOne(map);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e);
+            throw;
+        }
+
+        return Enumerable.Range(1, 1).Select(index => new Maps
             {
-                MapName = "Tavern",
-                CreatorId = "Blindy",
-                Id = "Blindy_1_PowerAdmin",
+                MapName = payloadData.mapName,
+                CreatorId = payloadData.creatorId,
+                Id = payloadData.id,
                 MapFile = fileByteArray
             })
             .ToArray();
     }
-    
+
     // [HttpGet("{id}")]
     // public async Task<ActionResult<Maps>> Get()
     // {
