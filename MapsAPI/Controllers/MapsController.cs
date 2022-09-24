78cff1cd91306cc2810f013367ad7898dc90e623
Hernan <whiterabbitbuilder@gmail.com>
2022-09-24 01:07:07 -0500

    Add search maps with exact query

--- MapsAPI/Controllers/MapsController.cs
+++ MapsAPI/Controllers/MapsController.cs
@@ -4,6 +4,7 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using Newtonsoft.Json;
 using MapsAPI.Models;
+using Newtonsoft.Json.Linq;
 
 namespace MapsAPI.Controllers;
 
@@ -95,7 +96,7 @@ public class MapsController : ControllerBase
             .Find(Builders<Maps>.Filter.Eq("_id", ObjectId.Parse(id)))
             .FirstOrDefaultAsync();
         var mapResponse = new
-        {   
+        {
             id = map.Id,
             mapName = map.MapName,
             creatorId = map.CreatorId,
@@ -106,12 +107,41 @@ public class MapsController : ControllerBase
     }
 
     [HttpPost("SearchMaps")]
-    public void SearchMaps()
+    public async Task<Object> SearchMaps()
     {
-        // var payload = String.Empty;
-        // return mapResults;
-    }
-    
-    
+        var client = new MongoClient("mongodb+srv://whiteRabbit:MaudioTest@cluster0.u4jq0.mongodb.net/test");
+        var database = client.GetDatabase("QH_Maps_Default");
+        var defaultMapsDb = database.GetCollection<Maps>("QH_Maps");
+        var payload = String.Empty;
+        var filter = Builders<Maps>.Filter;
+        // string searchText = String.Empty;
+        object? mapResults = null;
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            payload = reader.ReadToEndAsync().Result;
+        }
 
+        var payloadJSON = JsonConvert.DeserializeObject<dynamic>(payload);
+        if (payloadJSON.SearchText != null && payloadJSON.SearchText != "")
+        {
+            string searchText = payloadJSON.SearchText;
+            // filter = filter.Eq("mapName", payloadJSON.SearchText.ToString());
+            mapResults = await defaultMapsDb.Find(Builders<Maps>.Filter.Eq("mapName", searchText))
+                .ToListAsync();
+        }
+
+        var wrappedMapResults = new
+        {
+            maps = mapResults
+        };
+        // return new OkObjectResult(wrappedMapResults);
+        if (mapResults != null)
+        {
+            return mapResults;
+        }
+        else
+        {
+            return "Error, no map found";
+        }
+    }
 }
