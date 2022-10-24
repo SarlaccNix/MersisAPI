ad818f737dbd59dad1caed5e06429b32346da981
Hernan <whiterabbitbuilder@gmail.com>
2022-10-24 16:01:19 -0500

    Characters and Custom Objects Models & Char Controller WIP

---
+++ MapsAPI/Controllers/CharactersController.cs
@@ -0,0 +1,35 @@
+﻿using MapsAPI.Characters;
+
+namespace MapsAPI.Controllers;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+
+[ApiController]
+[Route("[controller]")]
+
+public class CharactersController
+{
+    [HttpPost("Characters")]
+    public IEnumerable<Character> newCharacter()
+    {
+        yield break;
+    }
+    [HttpGet("Characters")]
+    public IEnumerable<Character> getCharacters()
+    {
+        yield break;
+    }
+    [HttpPut("Characters")]
+    public IEnumerable<Character> updateCharacter()
+    {
+        yield break;
+    }
+    [HttpPost("SearchCharacters")]
+    public IEnumerable<Character> searchCharacter()
+    {
+        yield break;
+    }
+}
---
+++ MapsAPI/Controllers/CustomObjects.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using MapsAPI.Models;
+
+
+
+namespace MapsAPI.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class CustomObjects : Controller
+{
+}
