d6eb6af045ff33ee2873117f93007add03e479f4
Hernan <whiterabbitbuilder@gmail.com>
2022-12-08 18:07:39 -0500

    Modify Login endpoint and Add Username endpoint

--- MapsAPI/Controllers/UserController.cs
+++ MapsAPI/Controllers/UserController.cs
@@ -23,14 +23,14 @@ public class UserController : ControllerBase
         var database = client.GetDatabase("QuestHaven");
         var usersCollection = database.GetCollection<User>("Users");
         var payload = String.Empty;
-        
+
         using (StreamReader reader = new StreamReader(Request.Body))
         {
             payload = reader.ReadToEndAsync().Result;
         }
 
         User loginData = JsonConvert.DeserializeObject<User>(payload);
-        if (loginData == null || string.IsNullOrEmpty(loginData.id) || string.IsNullOrEmpty(loginData.username))
+        if (loginData == null || string.IsNullOrEmpty(loginData.id))
         {
             var error = new
             {
@@ -48,9 +48,9 @@ public class UserController : ControllerBase
             try
             {
                 var newUser = loginData;
-                Random randomizer = new Random();
-                string tagValue = randomizer.Next(1000, 9999).ToString();
-                newUser.qh_UserTag = newUser.username + "#" + tagValue;
+                // Random randomizer = new Random();
+                // string tagValue = randomizer.Next(1000, 9999).ToString();
+                // newUser.qh_UserTag = newUser.username + "#" + tagValue;
                 newUser.creation_Date_Time = DateTime.Now;
                 newUser.last_Login_Date_Time = DateTime.Now;
                 usersCollection.InsertOne(newUser);
@@ -68,11 +68,12 @@ public class UserController : ControllerBase
             {
                 var error = new
                 {
-                    error = "Error: "+e
+                    error = "Error: " + e
                 };
                 return new OkObjectResult(error);
             }
         }
+
         var update = Builders<User>.Update.Set("last_login_Date_Time", DateTime.Now);
         var fetchUpdate = await usersCollection.UpdateOneAsync(filter, update);
         var updatedLogin = new
@@ -84,5 +85,75 @@ public class UserController : ControllerBase
             last_Login_Date_Time = currentUser.last_Login_Date_Time
         };
         return new OkObjectResult(updatedLogin);
+    }
+
+    [HttpPost("username")]
+    public async Task<OkObjectResult> CreateQhUsername()
+    {
+        var database = client.GetDatabase("QuestHaven");
+        var usersCollection = database.GetCollection<User>("Users");
+        var payload = String.Empty;
+        User updatedData = new User();
+
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            payload = reader.ReadToEndAsync().Result;
+        }
+
+        User userData = JsonConvert.DeserializeObject<User>(payload);
+        if (userData == null || string.IsNullOrEmpty(userData.id) || string.IsNullOrEmpty(userData.username))
+        {
+            var error = new
+            {
+                error = "Error: missing id and/or User Tag"
+            };
+            return new OkObjectResult(error);
+        }
+
+        FilterDefinition<User> filter = Builders<User>.Filter.Eq("_id", userData.id);
+        var currentUser = await usersCollection
+            .Find(Builders<User>.Filter.Eq("_id", userData.id))
+            .FirstOrDefaultAsync();
+        if (currentUser == null)
+        {
+            var error = new
+            {
+                error = "Error: user does not exist, please login the user in order to create its index."
+            };
+            return new OkObjectResult(error);
+        }
+
+        if (!string.IsNullOrEmpty(userData.username))
+        {
+            Random randomizer = new Random();
+            string tagValue = randomizer.Next(1000, 9999).ToString();
+            currentUser.qh_UserTag = userData.username + "#" + tagValue;
+            var update = Builders<User>.Update.Set("qh_UserTag", currentUser.qh_UserTag).Set("username", userData.username);
+            var fetchUpdate = await usersCollection.UpdateOneAsync(filter, update);
+            if (fetchUpdate.MatchedCount == 1)
+            {
+                updatedData.id = userData.id;
+                updatedData.username = userData.username;
+                updatedData.qh_UserTag = currentUser.qh_UserTag;
+            }
+            else
+            {
+                var error = new
+                {
+                    error = "Error: user does not exist, please login the user in order to create its index."
+                };
+                return new OkObjectResult(error);
+            }
+
+
+        }
+
+        var updatedUser = new
+        {
+            id = updatedData.id,
+            username = updatedData.username,
+            qh_UserTag = updatedData.qh_UserTag,
+        };
+        return new OkObjectResult(updatedUser);
     }
 }
