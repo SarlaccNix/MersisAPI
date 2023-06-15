c4352c11fa661119f890ffbdcb5c976f73fba252
IMCE <israelcattan@hotmail.com>
2023-06-15 18:17:01 -0500

    -Added POST and PATCH function to upload and update character sheet templates and data

--- MapsAPI/Controllers/CharacterSheetController.cs
+++ MapsAPI/Controllers/CharacterSheetController.cs
@@ -22,6 +22,157 @@ public class CharacterSheetController : ControllerBase
             "mongodb+srv://doadmin:fuzs536H0R9P124y@db-mongodb-nyc1-91572-de834d8c.mongo.ondigitalocean.com/admin?tls=true&authSource=admin");
 
 
+
+    [HttpPost("characterSheetTemplate")]
+    public string NewCharacterSheetTemplate()
+    {
+
+        var database = client.GetDatabase(databaseName);
+        var templateCollection = database.GetCollection<CharacterSheetsTemplateModel>("QH_CharacterSheetsTemplates");
+        var payload = String.Empty;
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            payload = reader.ReadToEndAsync().Result;
+        }
+
+        CharacterSheetsTemplateModel sheetTemplateData = JsonConvert.DeserializeObject<CharacterSheetsTemplateModel>(payload);
+
+        if (sheetTemplateData == null)
+        {
+            return "Error: missing JSON data";
+        }
+
+        sheetTemplateData.creationDate= DateTime.Now;
+        sheetTemplateData.lastUpdate = DateTime.Now;
+         
+
+        try
+        {
+            templateCollection.InsertOne(sheetTemplateData);
+            return "Character Sheet Template added to Database";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e);
+            return "Error: " + e;
+        }
+
+    }
+
+    [HttpPatch("characterSheetTemplate")]
+    public async Task<string> UpdateCharacterSheetTemplate()
+    {
+
+        var database = client.GetDatabase(databaseName);
+        var templateCollection = database.GetCollection<CharacterSheetsTemplateModel>("QH_CharacterSheetsTemplates");
+        var payload = String.Empty;
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            payload = reader.ReadToEndAsync().Result;
+        }
+        CharacterSheetsTemplateModel sheetsTemplateModel = JsonConvert.DeserializeObject<CharacterSheetsTemplateModel>(payload);
+
+        if (sheetsTemplateModel == null)
+        {
+            return "Error: missing JSON data";
+        }
+
+
+        sheetsTemplateModel.lastUpdate = DateTime.Now;  
+
+        var sheetTemplate = Builders<CharacterSheetsTemplateModel>.Filter.Eq("id", sheetsTemplateModel.id);
+        var updateDefinition = Builders<CharacterSheetsTemplateModel>.Update
+            .Set("character_Sheet_Template", sheetsTemplateModel.characterSheetTemplate);
+
+        try
+        {
+            var update = await templateCollection.UpdateOneAsync(sheetTemplate, updateDefinition);
+            
+            return "Character sheet Template updated in Database";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e);
+            return "Error: " + e;
+        }
+
+    }
+
+    [HttpPost("characterSheetUserData")]
+    public string NewCharacterSheetUserData()
+    {
+
+        var database = client.GetDatabase(databaseName);
+        var sheetUserDataCollection = database.GetCollection<CharacterSheetData>("QH_CharacterSheets");
+        var payload = String.Empty;
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            payload = reader.ReadToEndAsync().Result;
+        }
+
+        CharacterSheetData sheetUserData = JsonConvert.DeserializeObject<CharacterSheetData>(payload);
+
+        if (sheetUserData == null)
+        {
+            return "Error: missing JSON data";
+        }
+
+        sheetUserData.creationDate = DateTime.Now;
+        sheetUserData.lastUpdate = DateTime.Now;
+
+
+        try
+        {
+            sheetUserDataCollection.InsertOne(sheetUserData);
+            return "Character Sheet user Data added to Database";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e);
+            return "Error: " + e;
+        }
+
+    }
+
+    [HttpPatch("characterSheetUserData")]
+    public async Task<string> UpdateCharacterSheetUserData()
+    {
+
+        var database = client.GetDatabase(databaseName);
+        var userDataCollection = database.GetCollection<CharacterSheetData>("QH_CharacterSheets");
+        var payload = String.Empty;
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            payload = reader.ReadToEndAsync().Result;
+        }
+        CharacterSheetData sheetData = JsonConvert.DeserializeObject<CharacterSheetData>(payload);
+
+        if (sheetData == null)
+        {
+            return "Error: missing JSON data";
+        }
+
+
+        sheetData.lastUpdate = DateTime.Now;
+
+        var sheetTemplate = Builders<CharacterSheetData>.Filter.Eq("id", sheetData.id);
+        var updateDefinition = Builders<CharacterSheetData>.Update
+            .Set("character_Sheet_User_Data", sheetData.characterSheetUserData);
+
+        try
+        {
+            var update = await userDataCollection.UpdateOneAsync(sheetTemplate, updateDefinition);
+
+            return "Character sheet data updated in Database";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e);
+            return "Error: " + e;
+        }
+
+    }
+
     [HttpPost("getCharacterSheetTemplateById")]
     public async Task<OkObjectResult> getCharacterSheetTemplate()
     {
